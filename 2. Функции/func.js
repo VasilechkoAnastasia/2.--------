811cc05666c88f37011caf48f64d744faa57de7d
Anastasia <Anastasia@gmail.com>
2024-09-23 16:17:41 +0300

    first commit

---
+++ 2. Функции/func.js
@@ -0,0 +1,9 @@
+function simpleFunction()
+{ 
+    document.write("Данная функция не принимает аргументов и не возвращает значение<br>");
+}
+function cube(n)
+{
+    let c = n * n * n; 
+    return c;  
+}
